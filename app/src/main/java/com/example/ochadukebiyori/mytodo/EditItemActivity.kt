af822967e0c8369dc58fc5c8ac2da03e9f725fc4
Pickles Ochaduke <pickles.ochazuke@gmail.com>
2020-03-20 13:13:13 +0900

    Todoアイテムを編集するActivityを追加

---
+++ app/src/main/java/com/example/ochadukebiyori/mytodo/EditItemActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ochadukebiyori.mytodo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditItemActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_item)
+    }
+}
