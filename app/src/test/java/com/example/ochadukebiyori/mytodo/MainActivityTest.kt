dfca35ef91e7ab1a9c16491930cd3e50924a534b
Pickles Ochaduke <pickles.ochazuke@gmail.com>
2020-05-05 00:21:14 +0900

    MainActivityでタスクの削除を行うテストを追加した

--- app/src/test/java/com/example/ochadukebiyori/mytodo/MainActivityTest.kt
+++ app/src/test/java/com/example/ochadukebiyori/mytodo/MainActivityTest.kt
@@ -1,53 +1,56 @@
 package com.example.ochadukebiyori.mytodo
 
+// android
 import android.app.Activity
-import android.widget.Button
+import android.app.Application
+import android.app.Instrumentation
+import android.content.ComponentName
+import android.content.Intent
 import android.widget.ListView
-import androidx.lifecycle.Lifecycle
-import androidx.test.core.app.ActivityScenario
-import org.junit.After
-import org.junit.Before
+
+// androidx.test
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
+// JUnit
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.junit.Rule
 
-import androidx.test.core.app.launchActivity
+// Espresso
 import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.ext.junit.rules.activityScenarioRule
-import com.google.common.truth.Truth.assertThat
-
+import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.Intents.intending
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasExtra
 
+// hamcrest
 import org.hamcrest.Matchers.*
 
-import org.junit.Rule
+import com.google.common.truth.Truth.assertThat
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
-    @get:Rule
-    val activityScenarioRule = activityScenarioRule<MainActivity>()
+//    @get:Rule
+//    val activityScenarioRule = activityScenarioRule<MainActivity>()
 
     @get:Rule
     val intentsTestRule = IntentsTestRule(MainActivity::class.java)
-
-    lateinit var scenario: ActivityScenario<MainActivity>
 
     lateinit var listView: ListView
 
     @Before
     fun setUp() {
-        scenario = activityScenarioRule.scenario
-        scenario.onActivity { activity ->
-            listView = activity.findViewById(R.id.todoList)
+        intentsTestRule.activity.apply {
+            listView = this.findViewById(R.id.todoList)
         }
     }
 
@@ -78,6 +81,39 @@ class MainActivityTest {
     }
 
     @Test
+    fun Deleteのインテントを受け取ると対象のアイテムを削除するべき() {
+
+        // Delete のインテントを作成する
+        val resultData = Intent()
+        resultData.putExtra("position", 1)
+        resultData.putExtra("event", "delete")
+
+        val result = Instrumentation.ActivityResult(Activity.RESULT_OK, resultData)
+        val componentName = ComponentName(
+                ApplicationProvider.getApplicationContext<Application>(),
+                EditItemActivity::class.java
+            )
+        intending(hasComponent(componentName)).respondWith(result)
+
+//        val editScenario = ActivityScenario.launch(EditItemActivity::class.java)
+//        editScenario.onActivity {
+//            it.findViewById<Button>(R.id.deleteButton).performClick()
+//        }
+//        editScenario.close()
+
+        // タスクを選択する
+//        val mainScenario = ActivityScenario.launch(MainActivity::class.java)
+
+        onData(allOf(`is`(instanceOf(String::class.java))))
+            .inAdapterView(withId(R.id.todoList))
+            .atPosition(1)
+            .perform(click())
+
+        // 結果を見る
+        assertThat(listView.count).isEqualTo(1)
+    }
+
+    @Test
     fun タスクの詳細で保存ボタンを押すと保存されるべき() {
         // タスクを選択する
         onData(allOf(`is`(instanceOf(String::class.java))))
@@ -95,7 +131,4 @@ class MainActivityTest {
 //
 //        assertThat(listView.count).isEqualTo(3)
     }
-
-    @Test
-    fun タスクの詳細で削除ボタンを押すと削除されるべき() {}
 }
